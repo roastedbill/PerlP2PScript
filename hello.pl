f082057be7d5b8a4db82860fd8a45cada24295e4
RoastedBill <roastedbill@gamil.com>
2014-05-20 09:43:09 +0800

    reading websites from a .txt file

--- hello.pl
+++ hello.pl
@@ -14,10 +14,35 @@ use constant true => 1;
 This Perl Script automatically scan and fetching executable files via torrent 
 =cut
 
+#global variables are defined here
 
-say "Hello World";
-my $name = <STDIN>;
-chomp $name;
-if(looks_like_number($name)){
-	say "hi $name, what's up?";
+my $fileLineCount = 0;
+my @fileWebsites;
+
+
+
+open(INFILE,$ARGV[0])
+	or die "Could not open file '$ARGV[0]' $!";;
+=pod
+=head1 Reading websites from a file
+
+This file including websites for the script to scan torrent files
+The name of the file is specified by the first command line parameter
+=cut
+
+#reading websites from the file
+while($fileWebsites[$fileLineCount] = <INFILE>){
+	$fileLineCount++;
 }
+
+if($fileLineCount == 0){
+	print STDERR "Could not open file\n";
+}
+
+foreach my $n (@fileWebsites){
+	say $n;
+}
+#chomp $name;
+#if(looks_like_number($name)){
+#	say "hi $name, what's up?";
+#}
