4b47ae34516d3757b74086f0eb507564cff8a28a
RoastedBill <roastedbill@gmail.com>
2014-05-21 14:26:00 +0800

    using Mojo

--- hello.pl
+++ hello.pl
@@ -2,10 +2,9 @@
 use 5.010;
 use strict;
 use warnings;
-use LWP::Simple;
-use LWP::UserAgent;
-use HTTP::Request;
-use HTTP::Response;
+use Mojo::DOM;
+use Mojo::UserAgent;
+use Mojo::URL;
 
 
 use constant false => 0;
@@ -22,13 +21,10 @@ This Perl Script automatically scan and fetching executable files via torrent
 
 my $fileLineCount = 0;
 my @fileWebsites;
-my $ua = LWP::UserAgent->new('IE 9');
-$ua->timeout(10);
+my $ua = Mojo::UserAgent->new;
+my %downloaded;
 my $torrentCount = 0;
 
-
-open(INFILE,$ARGV[0])
-	or die "Could not open file '$ARGV[0]' $!";;
 =pod
 =head1 Reading websites from a file
 
@@ -37,22 +33,26 @@ The name of the file is specified by the first command line parameter
 =cut
 
 #reading websites from the file
-while($fileWebsites[$fileLineCount] = <INFILE>){
-	chomp $fileWebsites[$fileLineCount];
-	scanWebsite($fileWebsites[$fileLineCount]); 
-	$fileLineCount++;
-}
 
-if($fileLineCount == 0){
-	print STDERR "Empty file\n";
-}
-
-#debug website list
-#foreach my $n (@fileWebsites){
-#	say $n;
+################## FILE IN ##################
+#open(INFILE,$ARGV[0])
+#	or die "Could not open file '$ARGV[0]' $!";;
+#
+#while($fileWebsites[$fileLineCount] = <INFILE>){
+#	chomp $fileWebsites[$fileLineCount];
+#	scanWebsite($fileWebsites[$fileLineCount]); 
+#	$fileLineCount++;
+#}
+#if($fileLineCount == 0){
+#	print STDERR "Empty file\n";
 #}
 
+################## FILE OUT ##################
+open(my $outputFile, '>', 'torrentLinks.txt');
 
+################## Main Function ##################
+scanWebsite('http://thepiratebay.se/browse/300');
+close $outputFile;
 ########## lvl_1 sub declarations come here ##########
 
 =pod
@@ -62,25 +62,42 @@ This function goes to the website indicated by the parameter and scan for execut
 =cut
 
 sub scanWebsite{
-	my $req = HTTP::Request->new(GET => @_);
-	my $response = $ua->request($req);
+	##### LWP Version #####
+	#my $req = HTTP::Request->new(GET => @_);
+	#my $response = $ua->request($req);
  	
- 	#debug
- 	say @_;
-	if ($response->is_success){
-   		print $response->content;  
-	}
-	else {
-    	print $response->status_line;
-	}
+	#if ($response->is_success){
+   	#	my $contents = $response->content; 
+ 		#say $contents;
+   		#print $outputFile, $parser;
+	#}
+	#else {
+    #	print $response->status_line;
+	#}
 	
+	##### Mojo version #####
+	my $contents = $ua->get(@_ => {DNT => 1}) -> res -> body;
+	chomp $contents;
+	my ($line,@lines) = split(/\n/,$contents);
+	foreach $line(@lines){
+		my $dom = Mojo::DOM -> new($line);
+	}
+  	#my $url;
+
+	#print "$url\n";
+	#print $outputFile "$url\n";
+
+    #$downloaded{$url} = 1;
+    #$torrentCount++;         
+
+
 	return;
-}
-
-sub activeWebsite{ 
-	
-
 }
 
 
 ########## lvl_2 sub declarations come here ##########
+=pod
+=head2 Paser Functions:
+
+These functions act as website-depended plugin to extract torrent links.
+=cut
