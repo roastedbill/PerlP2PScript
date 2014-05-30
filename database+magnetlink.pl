1db1af4b23a06d3f0e8516fd0ed03b227d1df620
RoastedBill <roastedbill@gmail.com>
2014-05-30 18:15:13 +0800

    Version0.1, magnet link version now support both piratebay and kickass.

--- database+magnetlink.pl
+++ database+magnetlink.pl
@@ -13,20 +13,22 @@ use Net::uTorrent;
 =head1 P2P Fetching Script
 =head2 Fuction:
 
-This Perl Script automatically scan and fetching executable files via torrent 
+This Perl Script automatically scan and fetching executable files via magnet links 
 =cut
 
 #global variables are defined here
 
 my $ua = Mojo::UserAgent->new;
-my $torrentCount=0;
-my $lastTimeCount=0;
-my $window=10; #add torrent to uTorrent every 10 torrent downloaded
-my $i=0;
-my $j=3;
 
-my $db = DBI->connect('dbi:mysql:torrentlinks', 'root', 'lrs19920827')
-or die "Connection Error $DBI::errstr\n";
+#####choose your website here#####
+my $website = 'KICKASS'; #'PIRATEBAY','KICKASS'
+my $title; 
+my $i;
+my $j;
+my $totalPages;
+my $currentPage;
+my $lastTimePage;
+my $window=10; #add torrent to uTorrent every 10 torrent downloaded
 
 =pod
 =head1 Reading webmagnetlinks from a file
@@ -41,15 +43,17 @@ The name of the file is specified by the first command line parameter
 
 ################## Main Function ##################
 
-for(;$j<5;$j++){
-	for ($i=0;$i<100;$i++){
-		scanWebsite("http://thepiratebay.se/browse/${j}00/$i/3");
-	}
+my $db = DBI->connect('dbi:mysql:torrentlinks', 'root', 'lrs19920827')
+or die "Connection Error $DBI::errstr\n";
+
+while(1)
+	scanWeb($website);
+	downloadFile();
+	$lastTimePage = $currentPage;
+	last if($currentPage == $totalPages);
 }
 
-downloadFile();
-
-say "All the files are added to uTorrent download queue successfully!";
+say "All the files from $website have been added to uTorrent download queue successfully!";
 $db->disconnect();
 
 ########## lvl_1 sub declarations come here ##########
@@ -61,32 +65,70 @@ This function goes to the webmagnetlink indicated by the parameter and scan for 
 All the links are stored into "torrentLinks" -> "piratebaymagnetlinks" (table)
 =cut
 
-sub scanWebsite{
-	my $pageN = 100*($j-3)+$i+1;
-	say "fetching from page $pageN out of 200";
-	my $dom = Mojo::DOM->new($ua->get(@_ => {DNT => 1}) -> res -> body);
-	my @urls =split(/\n/, $dom->find('[title="Download this torrent using magnet"]')->attr('href'));
-
-	while(@urls){
-		my $url = shift @urls;
-		my $sth1 = $db->prepare_cached('SELECT * FROM piratebaymagnetlinks WHERE magnetlink=?') 
-		or die "Couldn't prepare statement: " . $db->errstr;
-		$sth1->execute($url);
-		if (!$sth1->rows) {
-	    	print "$url\n";
-	    	my $sth2 = $db->prepare_cached('INSERT INTO piratebaymagnetlinks(magnetlink) VALUES(?)')
-	    	or die "Couldn't prepare statement: " . $db->errstr;
-	    
-	    	$sth2->execute($url);
-	    	$sth2->finish;
+sub scanWeb{
+	
+	if($_[0] eq 'PIRATEBAY'){
+		$title = "Download this torrent using magnet";
+		$totalPages = 200;
+		$lastTimePage = 0;
+		$j=3;
+		for(;$j<5;$j++){
+			for ($i=0;$i<100;$i++){
+				$currentPage = 100*($j-3)+$i+1;
+				scanWebsite("http://thepiratebay.se/browse/${j}00/$i/3");
+				if($currentPage - $lastTimePage >= $window){
+					return;
+				}
+			}
 		}
-		$sth1->finish;
+	}
+	elsif($_[0] eq 'KICKASS'){
+		$title = "Torrent magnet link";
+		$totalPages = 800;
+		$lastTimePage = 1;
+		for ($i=$lastTimePage;$i<401;$i++){
+				$currentPage = 2*$i-1;
+				scanWebsite("http://kickass.to/applications/$i/");
+				$currentPage = 2*$i;
+				scanWebsite("http://kickass.to/games/$i/");
+				last if($currentPage - $lastTimePage >= $window);
+			}
+
+	}
+	elsif($_[0] eq 'TORRENTFUNK'){
+
+	}
+	else{
+		say "$_[0] is not a website supported by this script, please chech again!";
+	}
+	return;
+}
+
+sub scanWebsite{
+	say "fetching from page $currentPage out of $totalPages from $website";
+	my $dom = Mojo::DOM->new($ua->get(@_ => {DNT => 1}) -> res -> body);
+	if ($dom){
+		my @urls =split(/\n/, $dom->find("[title=$title]")->attr('href'));
+
+		while(@urls){
+			my $url = shift @urls;
+			my $sth1 = $db->prepare_cached('SELECT * FROM piratebaymagnetlinks WHERE magnetlink=?') 
+			or die "Couldn't prepare statement: " . $db->errstr;
+			$sth1->execute($url);
+			if (!$sth1->rows) {
+	    		print "$url\n";
+	    		my $sth2 = $db->prepare_cached('INSERT INTO piratebaymagnetlinks(magnetlink) VALUES(?)')
+	    		or die "Couldn't prepare statement: " . $db->errstr;
+	    
+	    		$sth2->execute($url);
+	    		$sth2->finish;
+			}
+			$sth1->finish;
+		}
 	}
 
 	return;
 }
-
-
 
 
 ########## lvl_2 sub declarations come here ##########
