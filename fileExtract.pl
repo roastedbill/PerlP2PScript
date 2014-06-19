1a0eab0424e3e424db9923e06ef67f314b59d2ce
unknown <roastedbill@gmail.com>
2014-06-19 17:59:46 +0800

    support msi & exe now, not done for whole folder

--- fileExtract.pl
+++ fileExtract.pl
@@ -8,7 +8,8 @@ use Win32::Process;
 use Switch;
 #use DBI;
 
-my $fromDir = 'E:\uTorrentDownload'; #uTorrent download folder
+my $fromDir = 'C:\Users\Rensheng\Desktop\test'; #uTorrent download folder
+my @filesToExtract; #hold execytable files inside a folder
 
 #my $db = DBI->connect('dbi:mysql:torrentlinks', 'root', 'lrs19920827')
 #or die "Connection Error $DBI::errstr\n";
@@ -21,6 +22,7 @@ Win32::Process::Create($ProcessObj,
 		0,
 		NORMAL_PRIORITY_CLASS,
 		".")or print STDERR "couldn't exec autoInstaller";
+#$ProcessObj->Kill(0);
 
 #scan through download folder
 opendir(downloadFolder, "$fromDir") || die "Can't open directory $fromDir: $!\n";
@@ -29,6 +31,7 @@ closedir(downloadFolder);
 
 #check each download
 foreach my $fn (@list) {
+	@filesToExtract = ();
 	if($fn =~ m/^\./){ #drop out . & ..
 		next;
 	}else{
@@ -38,7 +41,7 @@ foreach my $fn (@list) {
 		#print "\$fp = $fp\n";
 		#print "\$fn = $fn\n";
 		#print "\$toDir = $toDir\n";	
-		if($fp =~ m/.*\.(exe|msi|iso|rar|zip)$/){ #for those not inside a folder
+		if($fp =~ m/.*\.(exe|msi|iso|rar|zip|cab)$/){ #for those not inside a folder
 			processFile($fp,'C:\Users\Rensheng\Desktop\try'); #$toDir
 		}else{
 			processFolder($fp,'C:\Users\Rensheng\Desktop\try'); #$toDir
@@ -47,27 +50,66 @@ foreach my $fn (@list) {
 	}
 }
 
+$ProcessObj->Kill(0);
 #$db->disconnect();
 
 #take 2 paras: file path & destination. 
 sub processFolder{
 	my $fileProcessed = 0; #num of successfully extracted files
 	my $fileFailed = 0; #num of files failed to extract
-	my @filesToExtract;
 	find(\&wanted, $_[0]);
 	foreach my $fp(@filesToExtract){
-		if(processFile($fp,'C:\Users\Rensheng\Desktop\try') == 0){
+		if(processFile($fp,$_[1]) == 0){
 			$fileProcessed++;
 		}else{
 			$fileFailed++;
 		}
+	}
+	say "fileProcessed: $fileProcessed";
+	say "fileFailed: $fileFailed";
+	return;
+}
+
+sub processFolderOnlyEXE{
+	find(\&wantedEXE, $_[0]);
+	foreach my $fp(@filesToExtract){
+		processFile($fp,$_[1]);
+	}
+	return;
+}
+
+sub processFolderOnlyRAR{
+	find(\&wantedRAR, $_[0]);
+	foreach my $fp(@filesToExtract){
+		processFile($fp,$_[0]);
 	}
 	return;
 }
 
 #search wanted files from a folder, write to @filesToExtract (full path)
 sub wanted {
-	if($File::Find::name =~ m/.*\.(exe|msi|iso|rar|zip)$/){
+	@filesToExtract = ();
+	if($File::Find::name =~ m/.*\.(exe|msi|iso|rar|zip|cab)$/){
+		$File::Find::name =~ s/\//\\/g;
+  		push @filesToExtract, $File::Find::name;
+  		return;
+	}
+	return;
+}
+
+sub wantedEXE{
+	@filesToExtract = ();
+	if($File::Find::name =~ m/.*\.exe$/){
+		$File::Find::name =~ s/\//\\/g;
+  		push @filesToExtract, $File::Find::name;
+  		return;
+	}
+	return;
+}
+
+sub wantedRAR{
+	@filesToExtract = ();
+	if($File::Find::name =~ m/.*\.(rar|zip|cab)$/){
 		$File::Find::name =~ s/\//\\/g;
   		push @filesToExtract, $File::Find::name;
   		return;
@@ -84,39 +126,96 @@ sub processFile{
 		case /.*\.zip$/	{ return processZIP($_[0],$_[1]) }
 		case /.*\.msi$/	{ return processMSI($_[0],$_[1]) }
 		case /.*\.exe$/	{ return processEXE($_[0],$_[1]) }
+		case /.*\.cab$/	{ return processCAB($_[0],$_[1]) }
 	}
 	return -1;
 }
 
 sub processISO{
-	my $tempF = $_[0];
-	$tempF =~ s/.iso$//;
-	if(extract($_[0],$tempF) == 0){
-		return 0;
+	my $ISOtemp = createExtractFolder($_[0],$_[1]);
+	if(extract($_[0],$ISOtemp) == 0){
+		say $_[1];
+		return processFolderOnlyEXE($ISOtemp,$_[1]);
+	}else{
+		### add something to indicate the extraction is not successful! ###
 	}
 }
 
 sub processRAR{
-
+	my $tempF = $_[0];
+	$tempF =~ s/.rar$//;
+	return extract($_[0],$tempF);
 }
 
 sub processZIP{
+	my $tempF = $_[0];
+	$tempF =~ s/.zip$//;
+	return extract($_[0],$tempF);
+}
 
+sub processCAB{
+	my $tempF = $_[0];
+	$tempF =~ s/.cab$//;
+	return extract($_[0],$tempF);
 }
 
 sub processMSI{
-
+	my $i = 0;
+	my $MSItemp = createExtractFolder($_[0],$_[1]);
+	while(1){
+		if($i<3 && is_folder_empty($MSItemp)){ #try 3 ways to extract (may overwrite) 
+			extract($_[0],$MSItemp);
+			$i++;
+		}
+		else{
+			processFolderOnlyRAR($MSItemp); #including rar, zip, cab, unzip to a folder under current folder
+			return copy($_[0],$_[1]);
+		}
+	}
 }
 
 sub processEXE{
-
+	my $i = 0;
+	my $EXEtemp = createExtractFolder($_[0],$_[1]);
+	while(1){
+		if($i<3 && is_folder_empty($EXEtemp)){ #try 3 ways to extract (may overwrite) 
+			extract($_[0],$EXEtemp);
+			$i++;
+			say $i;
+		}
+		else{
+			return copy($_[0],$_[1]);
+		}
+	}
 }
 
 #take 2 para: target file and destination, return 0 if success
 sub extract{
-	return system("UniExtract.exe  "."\"$_[0]\""." $_[1]" );
+	return system("UniExtract.exe  "."\"$_[0]\""." $_[1]");
 }
 
 sub copy{
 	return system("xcopy \"$_[0]\" $_[1] \/e \/i \/h");
+}
+
+sub is_folder_empty {
+    my $dirname = shift;
+    opendir(my $dh, $dirname) or die "$dirname Not a directory";
+    return scalar(grep { $_ ne "." && $_ ne ".." } readdir($dh)) == 0;
+}
+
+# take 2 paras: target path and destination path, return path of new folder to extract to
+sub createExtractFolder{
+	my @temp = split(/\\/,$_[0]);
+		my $fp; #name of the file
+		while(@temp){
+			$fp = shift @temp;
+		}
+		$fp = substr $fp, 0, -4;
+		say $fp;
+		$fp = $_[1] . '\\' . $fp; #new destination folder
+		unless(-d $fp){
+			mkdir $fp;
+		}
+	return $fp;
 }
